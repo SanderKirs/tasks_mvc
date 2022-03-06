682dbf33655cd6cd23105dd795a72745e8700488
SanderKirs <sander.kirs@khk.ee>
2022-03-06 14:33:47 +0200

    Created project basic view.

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 class Model {
     constructor() {
+        this.tasks = [
+            {id:1, text:'Be Good', complete:false},
+            {id:2, text:'Be Nice', complete:false}
+        ]
     }
 }
     class View {
         constructor() {
+            this.app = this.getElement('#root')
+            this.title = this.setElement('hl')
+            this.title.textContent = 'Tasks'
+            this.taskList = this.setElement('ul')
+            this.app.append(this.title, this.taskList)
+        }
+        getElement(selector){
+            const element = document.querySelector(selector)
+            return element
+        }
+        setElement(tag, classname){
+            const element = document.querySelector(tag)
+            if(classname !== undefined){
+                element.classlist.add(classname)
+            }
+            return element
         }
 }
 class Controller {
