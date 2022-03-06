d8d6235b8ef7d98c33f3af14e0f4ae89ff213710
SanderKirs <sander.kirs@khk.ee>
2022-03-06 14:56:11 +0200

    Added add task functionality.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//-----Model-----//
 class Model {
     constructor() {
         this.tasks = [
@@ -5,17 +6,44 @@ class Model {
             {id:2, text:'Be Nice', complete:false}
         ]
     }
+    addTask(taskText){
+        let id
+        if(this.tasks.length > 0) {
+            id = this.tasks[this.tasks.lenght - 1].id + 1
+        } else {
+            id = 1
+        }
+        const task = {
+            id: id, text: taskText, complete: false
+        }
+        this.tasks.push(task)
+        this.ifTaskListChanged(this.tasks)
+    }
+    taskListChanged(callback){
+        this.ifTaskListChanged = callback
+    }
 }
+
+//-----View-----//
     class View {
         constructor() {
             this.app = this.getElement('#root')
             this.title = this.setElement('hl')
             this.title.textContent = 'Tasks'
+            this.form = this.setElement('form')
+            this.input = this.setElement('input')
+            this.input.type = 'text'
+            this.input.placeholder = 'Add task'
+            this.submitButton = this.setElement('button')
+            this.submitButton.textContent = 'Add'
+            this.form.append(this.input, this.submitButton)
             this.taskList = this.setElement('ul')
-            this.app.append(this.title, this.taskList)
+            this.app.append(this.title, this.form,  this.taskList)
         }
-
         displayTasks(tasks){
+            while(this.taskList.firstChild){
+                this.taskList.removeChild(this.taskList.firstChild)
+            }
             tasks.forEach(task => {
                 const li = this.setElement('li')
                 li.id = task.id
@@ -24,12 +52,19 @@ class Model {
                 this.taskList.append(li)
             })
         }
-
+        addTask(handler){
+            this.form.addEventListener('submit', event => {
+                event.preventDefault()
+                if(this.input.value !== '') {
+                    handler(this.input.value)
+                    this.input.value = ''
+                }
+            })
+        }
         getElement(selector){
             const element = document.querySelector(selector)
             return element
         }
-
         setElement(tag, classname){
             const element = document.querySelector(tag)
             if(classname !== undefined){
@@ -38,14 +73,22 @@ class Model {
             return element
         }
 }
+
+//-----Controller-----//
 class Controller {
     constructor(model, view) {
         this.model = model
         this.view = view
+
+        this.model.taskListChanged(this.displayTasks)
+        this.view.addTask(this.handleAddTask)
         this.displayTasks(this.model.tasks)
     }
     displayTasks = tasks => {
         this.view.displayTasks(tasks)
+    }
+    handleAddTask = taskText => {
+        this.model.addTask(taskText)
     }
 }
 
